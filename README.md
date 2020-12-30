b6754010b2c741922a3e2f4fd79eaa1b8dffe309
Naiara Abaroa <naiaraabaroa@gmail.com>
2020-12-30 13:27:09 +0100

    Add december graph

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# weight-cycle-graph
+# Relationship between my weight and menstrual cycle
+
+Sideproject to track and learn about the relationship between my weight and menstrual cycle. 
+
+By the way I practice with [D3](https://www.d3-graph-gallery.com/index.html) graphs.
---
+++ js/main.js
@@ -0,0 +1,120 @@
+// I am using https://www.d3-graph-gallery.com/index.html
+
+var data1 = [
+  { date: 1, kilos: 56.1, cycle: 0 },
+  { date: 2, kilos: 55.5, cycle: 0 },
+  { date: 3, kilos: 56.2, cycle: 0 },
+  { date: 4, kilos: 56.2, cycle: 0 },
+  { date: 5, kilos: 56.8, cycle: 0 },
+  { date: 6, kilos: 55.4, cycle: 0 },
+  { date: 7, kilos: 55.7, cycle: 0 },
+  { date: 8, kilos: 56.1, cycle: 5 },
+  { date: 9, kilos: 55.8, cycle: 15 },
+  { date: 10, kilos: 55.3, cycle: 10 },
+  { date: 11, kilos: 55.9, cycle: 5 },
+  { date: 12, kilos: 55.5, cycle: 0 },
+  { date: 13, kilos: 55.5, cycle: 0 },
+  { date: 14, kilos: 56.2, cycle: 0 },
+  { date: 15, kilos: 55.6, cycle: 0 },
+  { date: 16, kilos: 55.9, cycle: 0 },
+  { date: 17, kilos: 55.6, cycle: 0 },
+  { date: 18, kilos: 55.7, cycle: 0 },
+  { date: 19, kilos: 55.7, cycle: 0 },
+  { date: 20, kilos: 54.9, cycle: 0 },
+  { date: 21, kilos: 55.4, cycle: 0 },
+  { date: 22, kilos: 56.1, cycle: 0 },
+  { date: 23, kilos: 55.3, cycle: 0 },
+  { date: 24, kilos: 55.5, cycle: 0 },
+  { date: 25, kilos: 55.5, cycle: 0 },
+  { date: 26, kilos: 56.4, cycle: 0 },
+  { date: 27, kilos: 56.4, cycle: 0 },
+  { date: 28, kilos: 56.4, cycle: 0 },
+  { date: 29, kilos: 55.3, cycle: 0 },
+  { date: 30, kilos: 55.1, cycle: 0 },
+  { date: 31, kilos: 55.3, cycle: 0 },
+];
+
+var margin = { top: 10, right: 30, bottom: 30, left: 50 },
+  width = 800 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+var svg = d3
+  .select("#graph")
+  .append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var x = d3.scaleLinear().range([0, width]);
+var xAxis = d3.axisBottom().scale(x);
+svg
+  .append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .attr("class", "myXaxis");
+
+var y = d3.scaleLinear().range([height, 0]);
+var yAxis = d3.axisLeft().scale(y);
+svg.append("g").attr("class", "myYaxis");
+
+var z = d3.scaleLinear().range([0, 1]);
+
+function update(data) {
+  x.domain([
+    0,
+    d3.max(data, function (d) {
+      return d.date;
+    }),
+  ]);
+  svg.selectAll(".myXaxis").call(xAxis);
+
+  y.domain([
+    54,
+    d3.max(data, function (d) {
+      return d.kilos;
+    }),
+  ]);
+  svg.selectAll(".myYaxis").call(yAxis);
+
+  var show = svg.selectAll(".lineTest").data([data], function (d) {
+    return d.date;
+  });
+
+  show
+    .enter()
+    .append("path")
+    .attr("class", "lineTest")
+    .merge(show)
+    .attr(
+      "d",
+      d3
+        .line()
+        .x(function (d) {
+          return x(d.date);
+        })
+        .y(function (d) {
+          return y(d.kilos);
+        })
+    )
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 2);
+
+  svg
+    .append("g")
+    .selectAll("dot")
+    .data(data1)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) {
+      return x(d.date);
+    })
+    .attr("cy", function (d) {
+      return y(d.kilos);
+    })
+    .attr("r", function (d) {
+      return z(d.cycle);
+    })
+    .attr("fill", "red");
+}
+update(data1);
